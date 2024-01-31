da9b051e9b4b18c72adba0d039bc744999b6e828
hermesonbastos <hermesonbastos@gmail.com>
2024-01-31 15:34:24 -0300

    primeiro commit

---
+++ 01_conceitos_do_vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 :style="{ textDecoration: decoration }">Hello {{name}}!</h1>
+    <input type="text" v-model="name">
+    <br>
+
+    <a :href="link">Link pro curso!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'Hermeson',
+    decoration: 'underline',
+    link: 'https://treinamento.vuejsbrasil.org'
+  })
+}
+
+</script>
+
+<style>
+
+</style>
