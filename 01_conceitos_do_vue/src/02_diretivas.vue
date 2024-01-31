f3e8b306c3b7dbca0c9f71b66d58c37046db989b
hermesonbastos <hermesonbastos@gmail.com>
2024-01-31 16:51:40 -0300

    diretivas

---
+++ 01_conceitos_do_vue/src/02_diretivas.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Minha lista de tarefas</h1>
+    <input type="text" v-focus>
+    <br>
+    <button @click="() => showList = !showList">Ver a lista</button>
+    <ul v-if="showList">
+      <li v-for="(task, index) in tasks" :key="`${task}-${index}`">
+        {{ task.name }}
+      </li>
+    </ul>
+    <p v-else>Lista de tarefas escondida</p>
+  </div>
+</template>
+
+<script>
+const focus = {
+  mounted: (el) => {
+    el.focus();
+  }
+}
+
+export default {
+  directives: {
+    focus
+  },
+  data: () => ({
+    showList: false,
+    tasks: [
+      { name: 'Fazer o curso', isDone: false },
+      { name: 'Fazer o lanche', isDone: false },
+      { name: 'Estudar', isDone: false },
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
--- 01_conceitos_do_vue/src/main.js
+++ 01_conceitos_do_vue/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './02_diretivas.vue'
 
 createApp(App).mount('#app')
